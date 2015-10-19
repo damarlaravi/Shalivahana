a9e016ca6c88aff71409d6931bece2020bb4985d
demarla ravi kumar <damarlaravi@gmail.com>
2015-10-19 10:21:48 +0530

    1019: added static impl in webapp-config.xml file

---
+++ src/main/java/com/shalivahana/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.shalivahana.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Ravi on 19/10/15.
+ **/
+
+@Controller
+@RequestMapping("/home")
+public class HomeController
+{
+    @RequestMapping("/home-view")
+    public String getHomeView(ModelMap modelMap) {
+        return "home/home-veiw";
+    }
+}
